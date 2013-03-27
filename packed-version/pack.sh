ec66ae32d04b891a094a25d2e2f202ce7d438f3e
Jager Mesh <jagermesh@gmail.com>
2013-03-27 10:59:21 -0400

    more leftovers removed

--- packed-version/pack.sh
+++ packed-version/pack.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd ..
-zip -x \*config.db.php\* -x \*packed-version\* -x \*.svn\* -x \*.git\* -x \*.DS_Store\* -x \*saved.json\* -x \*pack.sh\* -r install.zip ./* ./.htaccess
+zip -x \*config.db.php\* -x \*_logs\* -x \*packed-version\* -x \*.svn\* -x \*.git\* -x \*.DS_Store\* -x \*saved.json\* -x \*pack.sh\* -r install.zip ./* ./.htaccess
 mv install.zip ./packed-version/
