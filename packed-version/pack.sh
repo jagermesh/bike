2b698139502e043f386a7d07dc9ac3cc2c18c48a
Jager Mesh <jagermesh@gmail.com>
2013-03-27 00:24:38 -0400

    no message

--- packed-version/pack.sh
+++ packed-version/pack.sh
@@ -2,4 +2,4 @@
 
 cd ..
 zip -x \*config.db.php\* -x \*packed-version\* -x \*.svn\* -x \*.git\* -x \*.DS_Store\* -x \*saved.json\* -x \*pack.sh\* -r install.zip ./* ./.htaccess
-cp install.zip ./packed-version/
+mv install.zip ./packed-version/
